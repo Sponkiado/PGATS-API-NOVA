293d4fdbc61852ed38af03334ef8ddca73e0b802
Cristian Sponchiado <cristiansponki@hotmail.com>
2025-08-23 10:59:36 -0300

    Implementa autenticação

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const userController = require("./controllers/userController");
 const transferController = require("./controllers/transferController");
+const authenticateToken = require("./middleware/authMiddleware");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/users", userController);
-app.use("/transfers", transferController);
+app.use("/transfers", authenticateToken, transferController);
 
 module.exports = app;
--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -12,6 +12,7 @@ const transferService = require('../../services/transferService')
 // Testes
 describe("Transfer Controller", () => {
   describe("POST /transfers", () => {
+    
     it("Quando informo remetente e destinatario inexistentes recebo 400", async () => {
       const resposta = await request(app).post("/transfers").send({
         from: "cristian",
@@ -20,25 +21,58 @@ describe("Transfer Controller", () => {
       });
       expect(resposta.status).to.equal(400);
       expect(resposta.body).to.have.property('error', 'Usuário não encontrado');
-      
     });
-    it("Usando Mocks: Quando informo remetente e destinatario inexistentes recebo 400", async () => {
-        // mocar apenas a função transfer do service
-        const transferServiceMock = sinon.stub(transferService, 'transfer')
-        transferServiceMock.throws (new Error("Usuário não encontrado"));
 
-        const resposta = await request(app)
+    it("Usando Mocks: Quando informo remetente e destinatario inexistentes recebo 400", async () => {
+      // mocar apenas a função transfer do service
+      const transferServiceMock = sinon.stub(transferService, 'transfer')
+      transferServiceMock.throws (new Error("Usuário não encontrado"));
+
+      const resposta = await request(app)
         .post("/transfers")
         .send({
-            from: "cristian",
-            to: "carlos",
-            amount: 100,
+          from: "cristian",
+          to: "carlos",
+          amount: 100,
         });
+
       expect(resposta.status).to.equal(400);
       expect(resposta.body).to.have.property('error', 'Usuário não encontrado');
       
       // reseto o mock
       sinon.restore();
     });
+
+it.only("Usando Mocks: Quando informo valores válidos eu tenho sucesso com 201 CREATED", async () => {
+  // mock do service
+  const transferServiceMock = sinon.stub(transferService, 'transfer');
+  transferServiceMock.returns({
+    from: "cristian",
+    to: "carlos",
+    amount: 100,
+    date: new Date().toISOString()
+  });
+
+  const resposta = await request(app)
+    .post("/transfers")
+    .send({
+      from: "cristian",
+      to: "carlos",
+      amount: 100
+    });
+
+  expect(resposta.status).to.equal(201);
+
+  const respostaEsperada = require('../fixture/respostas/quandoInformoValoresValidosEuTenhoSucessoCom201Created.json');
+  delete resposta.body.date;
+  delete respostaEsperada.date;
+
+  expect(resposta.body).to.deep.equal(respostaEsperada);
+
+  // resetar o mock
+  sinon.restore();
+});
+
+
   });
 });
